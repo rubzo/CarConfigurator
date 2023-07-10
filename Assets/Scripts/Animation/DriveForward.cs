28f039dbdf54f26ae74ab1a61a588fbbd4001e05
Stephen Kyle <stephenckyle@gmail.com>
2023-07-10 09:20:26 +0100

    made the car physics a little bit more realistic?

--- Assets/Scripts/Animation/DriveForward.cs
+++ Assets/Scripts/Animation/DriveForward.cs
@@ -13,6 +13,25 @@ public class DriveForward : MonoBehaviour
 
     public void Go()
     {
-        myRigidBody.AddRelativeForce(Vector3.forward * 300.0f);
+        StartCoroutine(AccelerationCoroutine());
+    }
+
+    private IEnumerator AccelerationCoroutine()
+    {
+        int framesToAccelerateFor = 120;
+        int counter = 0;
+        while (counter != framesToAccelerateFor)
+        {
+            float lerp = counter / (float)framesToAccelerateFor;
+            lerp = EaseInSine(lerp);
+            myRigidBody.AddRelativeForce(Vector3.forward * (9.0f * lerp));
+            counter++;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private float EaseInSine(float x)
+    {
+        return 1 - Mathf.Cos((x * Mathf.PI) / 2);
     }
 }
