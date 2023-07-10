f9f4237c88d6024a0cc9c340eaae7bc5ffed0538
Stephen Kyle <stephenckyle@gmail.com>
2023-07-10 10:39:11 +0100

    make no refunds funnier

--- Assets/Scripts/Menu/InvoiceController.cs
+++ Assets/Scripts/Menu/InvoiceController.cs
@@ -9,6 +9,8 @@ public class InvoiceController : MonoBehaviour
     public TMP_Text priceListText;
     public TMP_Text totalPriceText;
 
+    public GameObject noRefundsText;
+
     public void CreateInvoice(List<string> itemNames, List<int> itemPrices)
     {
         itemListText.text = string.Join("\n", itemNames);
@@ -25,11 +27,20 @@ public class InvoiceController : MonoBehaviour
         priceListText.text = string.Join("\n", priceTexts);
 
         totalPriceText.text = string.Format("${0:#,#}", totalPrice);
+
+        noRefundsText.SetActive(false);
     }
 
     public void ShowInvoice()
     {
         gameObject.SetActive(true);
+        StartCoroutine(ShowNoRefundsAfterDelay());
+    }
+
+    private IEnumerator ShowNoRefundsAfterDelay()
+    {
+        yield return new WaitForSeconds(1.4f);
+        noRefundsText.SetActive(true);
     }
 
     public void HideInvoice()
